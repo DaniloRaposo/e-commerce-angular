18fb2af62df3821cb1e04ef0586b3ab7046d082c
Danilo Raposo <daniloraposocaldas@hotmail.com>
2024-03-13 08:44:15 -0300

    home (feat): init creation of home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Product } from '../../product';
+import { ModalComponent } from '../modal/modal.component';
+import { Modal } from '../../modal';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  products: Product[] = [];
+  modalData!: Modal;
+  @ViewChild(ModalComponent) modal!: ModalComponent;
 
+  constructor(private router: Router, private productService: ProductService) {}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  logout() {
+    localStorage.removeItem("token");
+    this.router.navigate(["/login"]);
+  }
+
+  createProduct() {
+    this.router.navigate(["/product"]);
+  }
+
+  async getProducts() {
+    const fetchProducts = await this.productService.getProducts();
+
+    if (typeof fetchProducts === "boolean") {
+      this.modalData = {
+        title: "Error",
+        type: "error",
+        content: "An error occur",
+      }
+      this.modal.open();
+      return;
+    }
+
+    this.products = fetchProducts;
+  }
 }
