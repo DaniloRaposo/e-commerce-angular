103e0c401496c85f72b6a70149c4eb056ba4e500
Danilo Raposo <daniloraposocaldas@hotmail.com>
2024-03-14 11:19:54 -0300

    router (feat): add product router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 import { isAuthenticated, isNotAuthenticated } from './services/route-permission.service';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent, title: "Login", canActivate: [isNotAuthenticated] },
   { path: 'signup', component: SignupComponent, title: "Sign up", canActivate: [isNotAuthenticated] },
+  { path: "product", component: ProductFormComponent, title: "Create Product", canActivate: [isAuthenticated] },
   { path: "", component: HomeComponent, title: "Home", canActivate: [isAuthenticated]}
 ];
